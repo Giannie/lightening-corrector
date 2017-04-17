327181a05e553a55dc8a359cc51020bcfff1349c
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2017-04-17 19:02:00 +0100

    Remove debug

--- lightning.py
+++ lightning.py
@@ -59,7 +59,6 @@ comment_queue = []
 then = 0
 print("Starting to trawl comments")
 for comment in reddit.subreddit('all').stream.comments():
-    print(comment.author)
     if username != str(comment.author) and foundWord(comment.body.lower()):
         print("Adding comment",comment,"to queue")
         comment_queue.append(comment)
