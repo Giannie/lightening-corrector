eeffaae108ec6c5ffc89accbee759da437f99e83
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2017-04-17 18:56:51 +0100

    Fix ==

--- lightning.py
+++ lightning.py
@@ -59,7 +59,7 @@ comment_queue = []
 then = 0
 print("Starting to trawl comments")
 for comment in reddit.subreddit('all').stream.comments():
-    if username str(comment.author) and foundWord(comment.body.lower()):
+    if username == str(comment.author) and foundWord(comment.body.lower()):
         print("Adding comment",comment,"to queue")
         comment_queue.append(comment)
     if time.time() - then > 60:
